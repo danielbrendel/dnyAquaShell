1f2eb5cc3ee647ef5b8072e30c8c7898ca4f5cad
Daniel Brendel <dbrendel1988@gmail.com>
2025-03-02 15:50:53 +0100

    #12 Fix boolean flags for keyboard event callback

--- defplugins/dx.window/dx.window/window.h
+++ defplugins/dx.window/dx.window/window.h
@@ -35,7 +35,7 @@ namespace DxWindow {
 
 		LRESULT KeyEvent(int vKey, bool bDown, bool bCtrlHeld, bool bShiftHeld, bool bAltHeld)
 		{
-			pShellPluginAPI->Scr_ExecuteCode(L"call " + this->m_sEvents.wszWndOnKeyEvent + L"(" + std::to_wstring(vKey) + L", " + (bDown ? L"true" : L"false") + +L", " + (bCtrlHeld ? L"true" : L"false") + L", " + (bShiftHeld ? L"true" : L"false") + L", " + (bAltHeld ? L"true" : L"false") + L") => void;");
+			pShellPluginAPI->Scr_ExecuteCode(L"call " + this->m_sEvents.wszWndOnKeyEvent + L"(" + std::to_wstring(vKey) + L", " + (bDown ? DXSTR_FLAG_TRUE : DXSTR_FLAG_FALSE) + +L", " + (bCtrlHeld ? DXSTR_FLAG_TRUE : DXSTR_FLAG_FALSE) + L", " + (bShiftHeld ? DXSTR_FLAG_TRUE : DXSTR_FLAG_FALSE) + L", " + (bAltHeld ? DXSTR_FLAG_TRUE : DXSTR_FLAG_FALSE) + L") => void;");
 			return 0;
 		}
 
